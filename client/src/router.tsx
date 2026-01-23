d1ba2525e62ce2991e78cd3730bf81089dea3fc4
Victor Varela <victor398@gmail.com>
2026-01-23 10:34:26 -0300

    Implement form handling in NewProduct view using React Router Data APIs; add action function and Form component for product registration

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./Layouts/Layout";
 import Products from "./views/Products";
-import NewProduct from "./views/NewProduct";
+import NewProduct, {action as newProductAction} from "./views/NewProduct";
 
 const router = createBrowserRouter([
   {
@@ -15,6 +15,7 @@ const router = createBrowserRouter([
       {
         path: "products/new",
         element: <NewProduct />,
+        action: newProductAction
       },
     ],
   },
@@ -24,6 +25,9 @@ export default router;
 
 /**
  * ELiminamos App.tsx porque ya no es la entrada de la aplicacion. Ahora es <Layout/> para ello en main.tsx usamos el RouterProvider
+ * 
  * - Agregamos children en la ruta para conectar Outlet con las vistas hijas del mismo Layout. Cuando visite la pagina principal '/' va a mostrar la vista de products, por eso es index:true. Entonces cada vez que se cambie el path, Outlet que esta en Layout se va a sustituir por el elmento que tiene router en ese path. La pagina principal esta en el path '/' ahi renderiza Layout-> Layout tiene hijos (rutas hijas), entonces cuando va a la principal (que la configuramos con index:true) renderiza Products que sustituye Outlet.
+ * 
+ * - Usamo React Router Data APIs: importamos la funcion action del componente NewProduct y la renombramos. Siempre las fucniones que manejan las ruta se llaman action y las debemos renombrar. import NewProduct, {action as newProductAction} from "./views/NewProduct";
  * 
  */
--- client/src/views/NewProduct.tsx
+++ client/src/views/NewProduct.tsx
@@ -1,5 +1,9 @@
-import { Link } from "react-router-dom"
+import { Link, Form } from "react-router-dom";
 
+export async function action() {
+  console.log("desde action...");
+  return {};
+}
 const NewProduct = () => {
   return (
     <>
@@ -12,8 +16,60 @@ const NewProduct = () => {
           Volver a Productos
         </Link>
       </div>
+      <Form className="mt-10" method="POST">
+        <div className="mb-4">
+          <label className="text-gray-800" htmlFor="name">
+            Nombre Producto:
+          </label>
+          <input
+            id="name"
+            type="text"
+            className="mt-2 block w-full p-3 bg-gray-50"
+            placeholder="Nombre del Producto"
+            name="name"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="text-gray-800" htmlFor="price">
+            Precio:
+          </label>
+          <input
+            id="price"
+            type="number"
+            className="mt-2 block w-full p-3 bg-gray-50"
+            placeholder="Precio Producto. ej. 200, 300"
+            name="price"
+          />
+        </div>
+        <input
+          type="submit"
+          className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
+          value="Registrar Producto"
+        />
+      </Form>
     </>
-  )
-}
+  );
+};
 
-export default NewProduct
+export default NewProduct;
+
+/**
+ * - Usamos Actions de react-router-dom para procesar formularios. Debemos importar Form y crear una funcion action. Importante siempre se llama action esa funcion y cuando la importamos en el router.tsx la renombramos como newProductAction.
+ * - Son 3 pasos:
+ *  1. Importar Form de react-router-dom--> permite el atributo method=''
+ *  2. Crear la funcion
+ *  3. En el router indicar cual es la funcion a ejecutar.
+ * 
+ * OJO: La funcion action vive FUERA del componente, el primer impulso fue escribirla dentro por lo aprendido en react clasico, Pero con React Router Data APIs el paradigma cambia:
+
+Ya no manejás eventos
+
+Delegás la lógica al router
+ * React Router impone esta idea:
+
+Componente → UI
+
+Loader / Action → datos y efectos
+- 
+ *
+ */
