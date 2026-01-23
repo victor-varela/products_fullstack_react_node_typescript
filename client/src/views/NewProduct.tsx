d7a5e7ca0001a602816a16a3b8b98caebd401d8c
Victor Varela <victor398@gmail.com>
2026-01-23 10:03:25 -0300

    Add routing and layout structure for Products and NewProduct views

--- client/src/views/NewProduct.tsx
+++ client/src/views/NewProduct.tsx
@@ -1,8 +1,18 @@
-
+import { Link } from "react-router-dom"
 
 const NewProduct = () => {
   return (
-    <div>From NewProduct</div>
+    <>
+      <div className="flex justify-between">
+        <h2 className="text-4xl font-black text-slate-500">Registrar Producto</h2>
+        <Link
+          to="/"
+          className="bg-indigo-600 rounded-md shadow-sm font-bold text-white p-3 text-sm hover:bg-indigo-500"
+        >
+          Volver a Productos
+        </Link>
+      </div>
+    </>
   )
 }
 
--- client/src/views/Products.tsx
+++ client/src/views/Products.tsx
@@ -1,9 +1,23 @@
-
+import { Link } from "react-router-dom";
 
 const Products = () => {
   return (
-    <div>Desde Products</div>
-  )
-}
+    <>
+      <div className="flex justify-between">
+        <h2 className="text-4xl font-black text-slate-500">Productos</h2>
+        <Link
+          to="/products/new"
+          className="bg-indigo-600 rounded-md shadow-sm font-bold text-white p-3 text-sm hover:bg-indigo-500"
+        >
+          Agregar Producto
+        </Link>
+      </div>
+    </>
+  );
+};
 
-export default Products
+export default Products;
+
+/**
+ * - Agregamos el routing con Link para navegar las views o pages. Importamos de react-router-dom : Link
+ */
