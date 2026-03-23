371ec9cfc9bc95a9880809d2b1e74ec37635e5e1
Victor Varela <victor398@gmail.com>
2026-03-23 16:46:03 -0300

    Refactor Layout and ProductDetails components: update layout structure, improve responsive text sizes, and ensure consistent padding in product details.

--- client/src/Layouts/Layout.tsx
+++ client/src/Layouts/Layout.tsx
@@ -2,16 +2,16 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <>
+    <div>
       <header className="bg-slate-800">
         <div className="mx-auto max-w-6xl py-10">
           <h1 className="text-4xl font-extrabold text-white">Administrador de Productos</h1>
         </div>
       </header>
-      <main className="mt-10 mx-auto max-w-6xl bg-white shadow p-10">
+      <main className="mt-10 mx-auto max-w-6xl bg-white shadow p-0 sm:p-6 lg:p-10">
         <Outlet />
       </main>
-    </>
+    </div>
   );
 };
 
